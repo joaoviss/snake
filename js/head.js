d0766b52f6db2c086e197af4b0f0d59b89600fad
joaoviss <joaoviss@gmail.com>
2023-06-25 22:51:41 -0300

    Update head.js

--- js/head.js
+++ js/head.js
@@ -12,28 +12,18 @@ class Head extends Drawable {
         ctx.fillStyle = '#f00'
         ctx.fill()
         ctx.stroke()
-
         ctx.drawImage(this.image, this.x * scale, this.y * scale, scale, scale)
     }
     update() {
-        /* 
-        if (this.speed.x < 0)
-        this.x += (this.x + this.speed.x >= 0) ? this.speed.x : grid.w - 1
-        if (this.speed.y < 0)
-        this.y += (this.y + this.speed.y >= 0) ? this.speed.y : grid.h - 1
-        if (this.speed.x > 0)
-        this.x += (this.x + this.speed.x < grid.w) ? this.speed.x : -(grid.w)
-        if (this.speed.y > 0)
-        this.y += (this.y + this.speed.y < grid.h) ? this.speed.y : -(grid.h)
-        /*/
-        if ((this.x > 0) ||
--        (this.y > 0) ||
-        (this.x <= grid.w) ||
-        (this.y <= grid.h)) {
+        if ((this.x > 0) || (this.y > 0) || (this.x <= grid.w) ||
+  (this.y <= grid.h)) {
             this.x += this.speed.x
             this.y += this.speed.y
-        } else this.gameover = true
+        }
         this.draw()
-        // */
+    }
+    reset() {
+        [this.x, this.y] = [0, 10]
+        this.speed = {1, 0}
     }
 }
