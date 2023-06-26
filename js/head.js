51c23a6a66908daf6ee78c83e57f582b0f837327
joaoviss <joaoviss@gmail.com>
2023-06-25 23:47:00 -0300

    Update head.js

--- js/head.js
+++ js/head.js
@@ -15,8 +15,8 @@ class Head extends Drawable {
         ctx.drawImage(this.image, this.x * scale, this.y * scale, scale, scale)
     }
     update() {
-        if ((this.x > 0) || (this.y > 0) || (this.x <= grid.w) ||
-  (this.y <= grid.h)) {
+        if ((this.x > 0) || (this.y > 0) || 
+            (this.x <= grid.w) || (this.y <= grid.h)) {
             this.x += this.speed.x
             this.y += this.speed.y
         }
@@ -24,6 +24,6 @@ class Head extends Drawable {
     }
     reset() {
         [this.x, this.y] = [0, 10]
-        this.speed = {1, 0}
+        this.speed = {x: 1, y: 0}
     }
 }
